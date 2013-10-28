8ba42ee1c44b704ce9bd368101ef8a3909eae4db
lopezurkidi <lopezurkidi@gmail.com>
2013-10-28 15:15:08 +0100

    Improve Spinner styling + examples

--- HoloAccentExample/src/com/negusoft/holoaccent/example/SpinnerActivity.java
+++ HoloAccentExample/src/com/negusoft/holoaccent/example/SpinnerActivity.java
@@ -6,30 +6,43 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.negusoft.holoaccent.ResourceHelper;
 
 public class SpinnerActivity extends Activity {
+
+	private static final String[] ITEMS = new String[] { "Item 1", "Item 2", "Item 3", "Item 4" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		getActionBar().setHomeButtonEnabled(true);
-		
+		initActionBar();
+		initSpinners();
+	}
+	
+	private void initActionBar() {
 		ActionBar actionBar = getActionBar();
+		
+		actionBar.setHomeButtonEnabled(true);
+		
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
 		actionBar.setDisplayShowTitleEnabled(false);
 		
-		String[] items = new String[] { "Item 1", "Item 2", "Item 3", "Item 4" };
-		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
-		actionBar.setListNavigationCallbacks(arrayAdapter, new ActionBar.OnNavigationListener() {
+		actionBar.setListNavigationCallbacks(new MyArrayAdapter(), new ActionBar.OnNavigationListener() {
 			@Override public boolean onNavigationItemSelected(int itemPosition, long itemId) {
 				return false;
 			}
 		});
-		
+	}
+	
+	private void initSpinners() {
+		Spinner spinner1 = (Spinner)findViewById(R.id.spinner1);
+		spinner1.setAdapter(new MyArrayAdapter());
+		Spinner spinner2 = (Spinner)findViewById(R.id.spinner2);
+		spinner2.setAdapter(new MyArrayAdapter());
 	}
 	
 	private final ResourceHelper mResourceHelper = new ResourceHelper();
@@ -42,6 +55,15 @@ public class SpinnerActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.spinner, menu);
 		return true;
+	}
+	
+	private class MyArrayAdapter extends ArrayAdapter<String> {
+		public MyArrayAdapter() {
+			super(SpinnerActivity.this, android.R.layout.simple_spinner_dropdown_item, ITEMS);
+		}
+		public MyArrayAdapter(int dropdownItemLayout) {
+			super(SpinnerActivity.this, dropdownItemLayout, ITEMS);
+		}
 	}
 
 }
