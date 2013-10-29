eb82a6d5df7eed7f1669e4368d5b8aed60218e90
lopezurkidi <lopezurkidi@gmail.com>
2013-10-29 14:38:18 +0100

    Add dark accent color to AccentPalette

--- HoloAccent/src/com/negusoft/holoaccent/AccentPalette.java
+++ HoloAccent/src/com/negusoft/holoaccent/AccentPalette.java
@@ -4,16 +4,18 @@ import android.graphics.Color;
 
 public class AccentPalette {
 	
+	private static final float DARK_ACCENT_PERCENTAGE = 0.85f;
+	
 	public final int accentColor;
 	public final int red;
 	public final int green;
 	public final int blue;
 	
 	public AccentPalette(int color) {
-		accentColor = color;
 		red = Color.red(color);
 		green = Color.green(color);
 		blue = Color.blue(color);
+		accentColor = Color.rgb(red, green, blue);
 	}
 	
 	public AccentPalette(int r, int g, int b) {
@@ -25,6 +27,17 @@ public class AccentPalette {
 	
 	public int getTranslucent(int alpha) {
 		return Color.argb(alpha, red, green, blue);
+	}
+	
+	public int getDarkAccentColor() {
+		return getDarkAccentColor(255);
+	}
+	
+	public int getDarkAccentColor(int alpha) {
+		int r = (int)(red * DARK_ACCENT_PERCENTAGE);
+		int g = (int)(green * DARK_ACCENT_PERCENTAGE);
+		int b = (int)(blue * DARK_ACCENT_PERCENTAGE);
+		return Color.argb(alpha, r, g, b);
 	}
 
 }
