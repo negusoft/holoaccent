50103fdbc5d7c00f6d5e51650ffaa7507556398d
lopezurkidi <lopezurkidi@gmail.com>
2013-11-19 17:26:08 +0100

    Add ListPopupWindow styling

--- HoloAccentExample/src/com/negusoft/holoaccent/example/fragment/ButtonFragment.java
+++ HoloAccentExample/src/com/negusoft/holoaccent/example/fragment/ButtonFragment.java
@@ -1,19 +1,62 @@
 package com.negusoft.holoaccent.example.fragment;
 
-import com.negusoft.holoaccent.example.R;
-import com.negusoft.holoaccent.example.R.layout;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListPopupWindow;
+
+import com.negusoft.holoaccent.example.R;
 
 public class ButtonFragment extends Fragment {
 	
+	private View mListPopupButton;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.buttons, null);
+		View result = inflater.inflate(R.layout.buttons, null);
+		
+		View coloredButton = result.findViewById(R.id.coloredButton);
+		coloredButton.setLongClickable(true);
+//		registerForContextMenu(coloredButton);
+		
+//		mListPopupButton = result.findViewById(R.id.listPopupButton);
+//		mListPopupButton.setOnLongClickListener(mPopupListener);
+		
+		return result;
 	}
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		if (v.getId() == R.id.coloredButton) {
+			MenuInflater inflater = getActivity().getMenuInflater();
+			inflater.inflate(R.menu.spinner, menu);
+		}
+	}
+	
+	private View.OnLongClickListener mPopupListener = new View.OnLongClickListener() {
+		@Override public boolean onLongClick(View v) {
+			String[] versions = { "Camera", "Laptop", "Watch", "Smartphone",
+					"Television" };
+			final ListPopupWindow listPopupWindow = new ListPopupWindow(
+					getActivity());
+			listPopupWindow.setAdapter(new ArrayAdapter<String>(getActivity(),
+					android.R.layout.simple_dropdown_item_1line, versions));
+			listPopupWindow.setAnchorView(mListPopupButton);
+			listPopupWindow.setWidth(300);
+			listPopupWindow.setHeight(400);
+
+			listPopupWindow.setModal(true);
+			listPopupWindow.show();
+			return false;
+		}
+	};
 
 }
--- HoloAccentExample/src/com/negusoft/holoaccent/example/fragment/ListFragment.java
+++ HoloAccentExample/src/com/negusoft/holoaccent/example/fragment/ListFragment.java
@@ -2,15 +2,22 @@ package com.negusoft.holoaccent.example.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.negusoft.holoaccent.example.R;
 
-public class ListFragment extends Fragment {
+public class ListFragment extends Fragment implements OnItemLongClickListener {
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -21,10 +28,44 @@ public class ListFragment extends Fragment {
 				android.R.layout.simple_list_item_1, 
 				getResources().getStringArray(R.array.list_items));
 		listView.setAdapter(adapter);
+		listView.setOnItemLongClickListener(this);
 		listView.setFastScrollEnabled(true);
 		listView.setFastScrollAlwaysVisible(true);
 		
 		return result;
 	}
+
+	@Override
+	public boolean onItemLongClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		Toast.makeText(getActivity(), "long click", Toast.LENGTH_SHORT).show();
+		getActivity().startActionMode(mActionModeCallback);
+		return false;
+	}
+	
+	private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
+
+		@Override
+		public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+			return true;
+		}
+
+		@Override
+		public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+	        MenuInflater inflater = mode.getMenuInflater();
+	        inflater.inflate(R.menu.spinner, menu);
+			return true;
+		}
+
+		@Override
+		public void onDestroyActionMode(ActionMode mode) {
+
+		}
+
+		@Override
+		public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+			return false;
+		}
+		
+	};
 
 }
