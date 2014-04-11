430e821c71749b372f19a0ad640bd35e29da7463
blurkidi <blurkidi@gmail.com>
2014-04-11 08:34:51 +0200

    Create pickers tab for showcasing picker widgets

--- HoloAccentExample/src/com/negusoft/holoaccent/example/fragment/PickersFragment.java
+++ HoloAccentExample/src/com/negusoft/holoaccent/example/fragment/PickersFragment.java
@@ -1,20 +1,26 @@
 package com.negusoft.holoaccent.example.fragment;
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.DatePicker;
 import android.widget.NumberPicker;
 
 import com.negusoft.holoaccent.example.R;
 
-public class ChoicesFragment extends Fragment {
+public class PickersFragment extends Fragment {
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.choices, null);
+		View result = inflater.inflate(R.layout.pickers, null);
+
+        NumberPicker nPicker1 = (NumberPicker)result.findViewById(R.id.numberPicker);
+        nPicker1.setMinValue(0);
+        nPicker1.setMaxValue(100);
+        nPicker1.setValue(30);
+
+        return result;
 	}
 
 }
